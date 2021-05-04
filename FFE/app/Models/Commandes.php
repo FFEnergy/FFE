e9d91b9e9f1f570f0c52922b91fbbd8ee10c1ca5
FlorentEden <fmartinez@edenschool.fr>
2021-05-04 11:23:45 +0200

    add migrations

---
+++ FFE/app/Models/Commandes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commandes extends Model
+{
+    use HasFactory;
+}
---
+++ FFE/database/migrations/2021_05_04_091610_create_produits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('Libelle_produit');
+            $table->string('Img_produit');
+            $table->integer('prix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
