13b18beae445a1a074e5e5a3b691dd2721bbe962
FlorentEden <fmartinez@edenschool.fr>
2021-05-12 15:27:28 +0200

    fixed css bug on aboutUS

--- FFE/public/JS/index.js
+++ FFE/public/JS/index.js
@@ -64,12 +64,3 @@ $(window).resize(function() {
     }, 200);
   }
 })
-
-var images = $( ".nuage" );
-var d1 = $(".nuage:nth-child(2)").css("left").split('p')[0];
-
-$(window).scroll(function(){
-  $(".nuage:nth-child(1)").css({"marginLeft": ($(window).scrollTop()) + "px"});
-  $(".nuage:nth-child(2)").css({"left": (d1 - $(window).scrollTop()) + "px"});
-  $(".nuage:nth-child(3)").css({"marginLeft": ($(window).scrollTop()) + "px"});
-});
